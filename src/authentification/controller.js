044529ff3d18b34f28564163399c096837bcda50
hamza <ikiouhamza@gmail.com>
2023-12-14 16:24:22 +0100

    add login service that check if email/password is provided, check if user exist and the password match, create an authentification token and put a refresh token in cookie section

--- src/authentification/controller.js
+++ src/authentification/controller.js
@@ -1,5 +1,31 @@
+const { getUserForLogin } = require('../user/controller')
+const { jwtTokens } = require("./jwt-helper")
+const bcrypt = require('bcryptjs')
 
-const login = async (req, res) => {}
+const login = async (req, res) => {
+    try {
+        const email = req.body.email
+        const password = req.body.password
+
+        // Check if an email and a password has been sent in the request
+        if (!email || !password) return res.status(401).json({error: 'Email or password was not provided.'})
+
+        // Check if the user exist and if the password in the database is the same that the one he sent
+        const user = await getUserForLogin(email)
+        const validPassword = bcrypt.compareSync(password, user.password)
+        if (!validPassword) return res.status(401).json({error: 'Password is incorrect.'})
+
+        // Create a JWT for the user
+        const paramsToken = { user_id: user.id, user_email: user.email, user_role: user.role }
+        const userJwt = jwtTokens(paramsToken)
+        res.cookie('refresh_token', userJwt.refreshToken, {httpOnly: true})
+
+        // Put in the response the JWT created
+        res.json(userJwt)
+    } catch (error) {
+        res.status(401).json({error: error.message})
+    }
+}
 
 const refresh_token = async (req, res) => {}
 
