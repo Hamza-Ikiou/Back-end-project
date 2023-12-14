93a7e16ddfbc33e72bb35e061f66b0bd0836f65c
hamza <ikiouhamza@gmail.com>
2023-12-14 15:58:57 +0100

    add create service for user with hash password

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,4 +1,5 @@
 const prisma = require("../prisma")
+const bcrypt = require('bcryptjs')
 
 const getAllUsers = async (req, res) => {}
 
@@ -6,7 +7,20 @@ const getUser = async (req, res) => {}
 
 const getUserForLogin = async (mail) =>  {}
 
-const createUser = async (req, res) => {}
+const createUser = async (req, res) => {
+    try {
+        const email = req.body.email
+        const password = bcrypt.hashSync(req.body.password, 8)
+        const role = req.body.role
+
+        const data = { email, password, role }
+        const user = await prisma.users.create({ data })
+        return res.json({ data: user })
+
+    } catch(error) {
+        return res.status(500).json({error: error.message})
+    }
+}
 
 const editUser = async (req, res) => {}
 
