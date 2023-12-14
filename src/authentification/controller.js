74a561417892cd63741e828c9cee055fcc6bd8ae
hamza <ikiouhamza@gmail.com>
2023-12-14 16:22:11 +0100

    add todo for login and refresh token in controller authentification

---
+++ src/authentification/controller.js
@@ -0,0 +1,8 @@
+
+const login = async (req, res) => {}
+
+const refresh_token = async (req, res) => {}
+
+const delete_token = async (req, res) => {}
+
+module.exports = { login, refresh_token, delete_token }
