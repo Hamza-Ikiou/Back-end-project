1ff8db8b5fdd19a759a16027f726088fac491eff
hamza <ikiouhamza@gmail.com>
2023-12-20 14:43:25 +0100

    route restriction

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,16 +1,16 @@
 const {Router} = require('express');
 const { getAllUsers, getUser, createUser, createUserForRegistration, editUser, deleteUser } = require('./controller');
-
+const { authenticateToken, roleAuthorization, userAuthorization } = require('../middleware/authorization');
 const userRouter = new Router();
 
 userRouter.route('/users')
-    .get(getAllUsers)
-    .post(createUser)
+    .get(authenticateToken, roleAuthorization(process.env.ROLE_ADMIN), getAllUsers)
+    .post(authenticateToken, roleAuthorization(process.env.ROLE_ADMIN), createUser)
 
 userRouter.route('/user/:email')
-    .get(getUser)
-    .put(editUser)
-    .delete(deleteUser)
+    .get(authenticateToken, roleAuthorization(process.env.ROLE_USER), userAuthorization, getUser)
+    .put(authenticateToken, roleAuthorization(process.env.ROLE_USER), userAuthorization, editUser)
+    .delete(authenticateToken, roleAuthorization(process.env.ROLE_ADMIN), deleteUser)
 
 userRouter.route('/user/register')
     .post(createUserForRegistration)
